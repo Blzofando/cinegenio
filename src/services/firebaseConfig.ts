0b19ee00dbd94e370f0aa6350fe98aa51ab6cef5
blzofando <alecust123@gmail.com>
2025-08-23 19:06:22 -0300

    update 98

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,20 +1,30 @@
-// services/firebaseConfig.ts
+// src/services/firebaseConfig.ts
 
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import * as admin from "firebase-admin";
 
-// TODO: Substitua o objeto abaixo pelas credenciais do SEU projeto Firebase
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
+const isServer = typeof window === 'undefined';
 
-// Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
+if (isServer) {
+    if (!admin.apps.length) {
+        const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY as string);
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+        });
+    }
+} else {
+    if (!getApps().length) {
+        const firebaseConfig = {
+            apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+            authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+            projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+            storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+            appId: import.meta.env.VITE_FIREBASE_APP_ID
+        };
+        initializeApp(firebaseConfig);
+    }
+}
 
-// Exporta a instância do banco de dados para usarmos em outros lugares
-export const db = getFirestore(app);
+export const db = isServer ? admin.firestore() : getFirestore();
